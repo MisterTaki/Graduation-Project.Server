509f0b5b8906fedff855f96f6d48f5b336d027b5
MisterTaki <ustb_gaoqi@163.com>
2017-04-18 17:36:44 +0800

    :fries: jwt -> 'helpers/JWT';

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -1,24 +1,23 @@
-import jwt from 'jsonwebtoken';
 import unless from 'express-unless';
-import { JWT } from '../config';
-import { APIError } from '../helpers';
+import { APIError, JWT } from '../helpers';
 
 /* eslint consistent-return: 0 */
 function checkToken (req, res, next) {
   const token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers.authorization; // headers' attribute-name are lower-case;
   if (token) {
-    jwt.verify(token, JWT.secret, (err, { _id, identity }) => {
-      if (err) {
-        if (err.name === 'TokenExpiredError') {
-          return next(new APIError('登录信息已过期，请重新登录', 401));
-        }
-        return next(new APIError('登录信息已无效，请重新登录', 401));
-      }
+    JWT.verify(token)
+    .then(({ _id, identity }) => {
       req.user = {
         _id,
         identity
       };
       return next();
+    })
+    .catch((err) => {
+      if (err.name === 'TokenExpiredError') {
+        return next(new APIError('登录信息已过期，请重新登录', 401));
+      }
+      return next(new APIError('登录信息已无效，请重新登录', 401));
     });
   } else {
     return next(new APIError('没有登录信息，请重新登录', 401));
