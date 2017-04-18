cee3a8b37b945e328b9d94ff0c81978c4d31b9f9
MisterTaki <ustb_gaoqi@163.com>
2017-04-18 23:12:39 +0800

    :radio: add apply_student;

--- src/models/user.js
+++ src/models/user.js
@@ -115,19 +115,59 @@ const adminSchema = mongoose.Schema({
   mobile: String
 });
 
+const applyStudentSchema = mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  academy: {
+    type: String,
+    required: true
+  },
+  ID: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: true
+  },
+  class: {
+    type: String,
+    required: true
+  },
+  major: {
+    type: String,
+    required: true
+  }
+});
+
 const statics = {
   getById (id) {
     return new Promise((resolve, reject) => {
       this.findById(id).exec().then((user) => {
         if (user) resolve(user);
-        reject(new APIError('账号不存在'));
+        reject(new APIError('账号不存在或未申请'));
       });
     });
   },
   notUserById (id) {
     return new Promise((resolve, reject) => {
       this.findById(id).exec().then((user) => {
-        if (user) reject(new APIError('账号已存在'));
+        if (user) reject(new APIError('账号已存在或已申请'));
         resolve();
       });
     });
@@ -137,9 +177,11 @@ const statics = {
 studentSchema.statics = statics;
 teacherSchema.statics = statics;
 adminSchema.statics = statics;
+applyStudentSchema.statics = statics;
 
 export default {
   Student: mongoose.model('Student', studentSchema),
   Teacher: mongoose.model('Teacher', teacherSchema),
   Admin: mongoose.model('Admin', adminSchema),
+  ApplyStudent: mongoose.model('Apply_Student', applyStudentSchema),
 };
