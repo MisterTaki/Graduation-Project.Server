e409e786821664977c13aa2befacb10a0df66233
MisterTaki <ustb_gaoqi@163.com>
2017-04-21 17:11:34 +0800

    :bulb: add set-pwd;

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/create', user.create);
 router.post('/apply', user.apply);
 router.post('/forget-pwd', user.forgetPwd);
+router.post('/set-pwd', user.setPwd);
 
 export default router;
