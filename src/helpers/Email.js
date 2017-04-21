4ac81279601d47bc04120bb436a53647541e23b4
MisterTaki <ustb_gaoqi@163.com>
2017-04-21 17:10:21 +0800

    :nut_and_bolt: update;

--- src/helpers/Email.js
+++ src/helpers/Email.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import { Captcha } from '../models';
 import { APIError } from '../helpers';
 import { createCaptcha } from '../utils';
 import { email as emailConfig } from '../config';
@@ -7,17 +8,23 @@ const transporter = nodemailer.createTransport(emailConfig);
 
 function resetPwd (toEmail) {
   return new Promise((resolve, reject) => {
+    const captchaCode = createCaptcha();
     const mailOptions = {
       from: `毕业设计（论文）管理系统<${emailConfig.auth.user}>`,
       to: toEmail,
-      subject: '验证码',
-      text: `您的验证码是：${createCaptcha()}（区分大小写），将于5分钟后失效。`
+      subject: `您此次重置密码的验证码是：${captchaCode}（区分大小写）`,
+      text: `您此次重置密码的验证码是：${captchaCode}（区分大小写），请在 30 分钟内输入验证码进行下一步操作。 如非你本人操作，请忽略此邮件。`
     };
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         return reject(new APIError('发送验证码失败'));
       }
-      return resolve(info);
+      return Captcha.create({
+        code: captchaCode,
+        email_address: toEmail
+      })
+      .then(() => resolve(info))
+      .catch(err => reject(err));
     });
   });
 }
