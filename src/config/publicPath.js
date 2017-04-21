3faac25e87c460c439f9029bcc0519bdf1b6edb3
MisterTaki <ustb_gaoqi@163.com>
2017-04-21 17:09:22 +0800

    :nut_and_bolt: add set-pwd;

--- src/config/publicPath.js
+++ src/config/publicPath.js
@@ -5,5 +5,6 @@ const { URIprefix } = version;
 export default [
   `${URIprefix}/auth/login`,
   `${URIprefix}/user/apply`,
-  `${URIprefix}/user/forget-pwd`
+  `${URIprefix}/user/forget-pwd`,
+  `${URIprefix}/user/set-pwd`
 ];
