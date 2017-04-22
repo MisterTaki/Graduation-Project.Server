10ee6e39d312f7ecd9334326667a1a3d02210ff6
MisterTaki <ustb_gaoqi@163.com>
2017-04-22 17:38:06 +0800

    :low_brightness: update;

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 export JWT from './JWT';
 export mongoDB from './mongoDB';
-export system from './system';
 export version from './version';
 export publicPath from './publicPath';
 export email from './email';
