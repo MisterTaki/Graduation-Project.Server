7aa487db6ff6ceb68d0a3327f0abedc1221c012c
MisterTaki <ustb_gaoqi@163.com>
2017-04-23 23:47:58 +0800

    :gem: update;

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,9 +3,11 @@ import { user } from '../controllers';
 
 const router = express.Router();
 
+router.get('/load', user.load);
 router.post('/create', user.create);
 router.post('/apply', user.apply);
 router.post('/forget-pwd', user.forgetPwd);
 router.post('/set-pwd', user.setPwd);
+router.post('/modify-pwd', user.modifyPwd);
 
 export default router;
