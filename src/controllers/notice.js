c16e2d746804fbbcb4b33db4028d58ea0ee49423
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 20:10:34 +0800

    :octocat: add load, remove;

--- src/controllers/notice.js
+++ src/controllers/notice.js
@@ -4,10 +4,10 @@ import { Notice, User } from '../models';
 
 function publish ({ body, user }, res, next) {
   const { _id, identity } = user;
-  const { title, content, remark } = body;
   if (identity !== 'admin') {
     return next(new APIError('不是管理员，无法发布', 401));
   }
+  const { title, content, remark } = body;
   return User[titleCase(identity)].getUserById(_id)
     .then(({ username }) => Notice.create({
       title,
@@ -15,6 +15,33 @@ function publish ({ body, user }, res, next) {
       remark,
       author: username
     }))
+    .then(notice => res.json({
+      ...successRes,
+      result: {
+        notice
+      }
+    }))
+    .catch(next);
+}
+
+function load (req, res, next) {
+  return Notice.find({}, 'title content remark author created_at').sort('-_id').exec()
+    .then(noticeList => res.json({
+      ...successRes,
+      result: {
+        noticeList
+      }
+    }))
+    .catch(next);
+}
+
+function remove ({ body, user }, res, next) {
+  const { identity } = user;
+  if (identity !== 'admin') {
+    return next(new APIError('不是管理员，无法删除', 401));
+  }
+  const { _id } = body;
+  return Notice.findByIdAndRemove(_id).exec()
     .then(() => res.json({
       ...successRes
     }))
@@ -22,5 +49,7 @@ function publish ({ body, user }, res, next) {
 }
 
 export default {
-  publish
+  publish,
+  load,
+  remove
 };
