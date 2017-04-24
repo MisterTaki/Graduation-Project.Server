6e4acdef864f3bed53365321dfc19b72c36452de
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 15:31:17 +0800

    :soccer: add 'modify-email', 'set-email';

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,5 +9,7 @@ router.post('/apply', user.apply);
 router.post('/forget-pwd', user.forgetPwd);
 router.post('/set-pwd', user.setPwd);
 router.post('/modify-pwd', user.modifyPwd);
+router.post('/modify-email', user.modifyEmail);
+router.post('/set-email', user.setEmail);
 
 export default router;
