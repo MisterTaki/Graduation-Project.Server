edb7342f2e6da610cf4296b3e1e0d882bc4e50ab
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 20:10:44 +0800

    :octocat: add load, remove;

--- src/routes/notice.js
+++ src/routes/notice.js
@@ -4,5 +4,7 @@ import { notice } from '../controllers';
 const router = express.Router();
 
 router.post('/publish', notice.publish);
+router.get('/load', notice.load);
+router.delete('/remove', notice.remove);
 
 export default router;
