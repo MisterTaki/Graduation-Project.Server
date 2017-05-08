0ece586c4e6f0604b7c84c13db0cef1cdbfe7b75
MisterTaki <ustb_gaoqi@163.com>
2017-05-08 14:30:04 +0800

    :black_nib: add 'findByIdAndRemove'

--- src/models/Notice.js
+++ src/models/Notice.js
@@ -13,7 +13,21 @@ const noticeSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  remark: String
+  remark: String,
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
 });
+
+noticeSchema.statics = {
+  findByIdAndRemove (_id) {
+    return new Promise((resolve, reject) => {
+      this.findOneAndUpdate({ _id }, { isDeleted: true }).exec()
+      .then(() => resolve())
+      .catch(err => reject(err));
+    });
+  }
+};
 
 export default mongoose.model('notice', noticeSchema);
