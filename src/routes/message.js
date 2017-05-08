5476258a02e80e156c9e2f1d9dd49fe65893658d
MisterTaki <ustb_gaoqi@163.com>
2017-05-08 14:29:40 +0800

    'updateMany' add 'delete', 'mark';

--- src/routes/message.js
+++ src/routes/message.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get('/load', message.load);
 router.post('/send', message.send);
+router.delete('/delete', message.remove);
+router.post('/mark', message.mark);
 
 export default router;
