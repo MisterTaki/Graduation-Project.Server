c46c94e3dca3edd5653c1fc6db1c2e2a8c7e1c31
MisterTaki <ustb_gaoqi@163.com>
2017-05-09 10:51:47 +0800

    :headphones: update;

--- src/controllers/volunteer.js
+++ src/controllers/volunteer.js
@@ -1,25 +1,21 @@
-// import { APIError } from '../helpers';
+import { APIError } from '../helpers';
 import { successRes } from '../utils';
 import { User } from '../models';
 
-function load ({ user }, res, next) {
-  const { _id, identity } = user;
-  let getMethod;
-  switch (identity) {
-    case 'student':
-      getMethod = User.Teacher.getVolunteerTeachers.bind(User.Teacher);
-      break;
-    default:
-
-  }
-  return getMethod(_id)
-    .then(teahcers => res.json({
+function load ({ user, query }, res, next) {
+  const { identity } = user;
+  const { type } = query;
+  if (identity === 'student' && type === 'option') {
+    return User.Teacher.getVolunteerTeachers()
+    .then(teachers => res.json({
       ...successRes,
       result: {
-        teahcers
+        teachers
       }
     }))
     .catch(next);
+  }
+  return next(new APIError('参数格式错误', 400));
 }
 
 export default {
