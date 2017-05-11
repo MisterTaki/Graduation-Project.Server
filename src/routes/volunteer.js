50d00b8fdc7b04f13f182a88e04e00fcddc5bc10
MisterTaki <ustb_gaoqi@163.com>
2017-05-11 14:12:03 +0800

    :partly_sunny: add 'refuse';

--- src/routes/volunteer.js
+++ src/routes/volunteer.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/load', volunteer.load);
 router.post('/choose', volunteer.choose);
+router.post('/refuse', volunteer.refuse);
 
 export default router;
