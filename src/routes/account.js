e6fe2d722f6b57c6e4a2baebc2fddd732b2ea6ac
MisterTaki <ustb_gaoqi@163.com>
2017-05-12 19:16:41 +0800

    :musical_keyboard: update;

--- src/routes/account.js
+++ src/routes/account.js
@@ -3,7 +3,9 @@ import { account } from '../controllers';
 
 const router = express.Router();
 
+router.post('/create', account.create);
 router.get('/load', account.load);
 router.post('/modify', account.modify);
+router.delete('/delete', account.remove);
 
 export default router;
--- src/routes/system.js
+++ src/routes/system.js
@@ -4,6 +4,7 @@ import { system } from '../controllers';
 const router = express.Router();
 
 router.get('/load', system.load);
+router.get('/obtain', system.obtain);
 router.post('/modify', system.modify);
 
 export default router;
