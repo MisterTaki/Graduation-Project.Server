a68a37b70d5b15b6934a0dc8d46ad6fe6823945e
MisterTaki <ustb_gaoqi@163.com>
2017-05-12 20:37:13 +0800

    :euro: update;

--- src/routes/account.js
+++ src/routes/account.js
@@ -4,6 +4,8 @@ import { account } from '../controllers';
 const router = express.Router();
 
 router.post('/create', account.create);
+router.post('/agree', account.agree);
+router.post('/refuse', account.refuse);
 router.get('/load', account.load);
 router.post('/modify', account.modify);
 router.delete('/delete', account.remove);
